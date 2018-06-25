f0044050b3e0bbc6b6879af19cd7976f0a10f478
Denise Alejandra Rodriguez Silva <denise.rodriguez@neoris.cxnetworks.net>
2018-06-25 11:12:44 -0500

    Servivio2

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router} from '@angular/router';
+import { ArticleService } from '../article.service';
 
 @Component({
   selector: 'app-article',
@@ -10,19 +11,23 @@ import { Router} from '@angular/router';
 })
 export class ArticleComponent implements OnInit {
   id: number;
+  articleComponent = [];
   
-  articles= [
-    { nombre: 'Parabola cero', desc: 'Descripcion', image: 'http://jsequeiros.com/sites/default/files/imagen-cachorro-comprimir.jpg', bandera: 0 },
-    { nombre: 'Parabola uno', desc: 'Descripcion', image: 'http://jsequeiros.com/sites/default/files/imagen-cachorro-comprimir.jpg', bandera: 0 },
-    { nombre: 'Parabola dos', desc: 'Descripcion', image: 'http://jsequeiros.com/sites/default/files/imagen-cachorro-comprimir.jpg', bandera: 0 }
-  ];
+  /*articles= [
+    { nombre: 'Pergola cero',desc: 'Descripcion', image: 'http://jsequeiros.com/sites/default/files/imagen-cachorro-comprimir.jpg', bandera: 0 },
+    { nombre: 'Pergola uno', desc: 'Descripcion', image: 'http://jsequeiros.com/sites/default/files/imagen-cachorro-comprimir.jpg', bandera: 0 },
+    { nombre: 'Pergola dos', desc: 'Descripcion', image: 'http://jsequeiros.com/sites/default/files/imagen-cachorro-comprimir.jpg', bandera: 0 }
+  ];*/
 
   navegar(i2: string){
     this.router.navigate(['auxiliar', i2]);
   }
 
-  constructor(public router : Router) { 
-    
+  constructor(public router : Router, public articleSevice: ArticleService) { 
+    this.articleComponent = articleSevice.getArticles();
+    console.log(this.articleComponent);
+    //this.id = this.activatedRoute.snapshot.params['id'];
+    //console.log(this.id);
   }
      
   ngOnInit() {
